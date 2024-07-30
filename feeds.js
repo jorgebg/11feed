cef7f92e9900459042ba180b23ee5212215fb2af
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2024-07-30 12:43:48 +0200

    Add user agent

--- feeds.js
+++ feeds.js
@@ -139,7 +139,13 @@ const parseFeed = async (url) => {
 
 	let rawFeed = await EleventyFetch(url, {
 		duration: "1d",
-		type: "text"
+		type: "text",
+		fetchOptions: {
+			headers: {
+				"user-agent":
+				"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+			},
+		},
 	});
 
 
